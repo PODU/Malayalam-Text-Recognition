4430c9e53a6d40d36595e6f349708a5ecc0f755e
Aditya Poduval <podu2997@gmail.com>
2019-08-24 15:38:19 +0530

    Remaining File a and folders to complete all the online files

---
+++ Python/Main.py
@@ -0,0 +1,310 @@
+import os
+import shutil
+import Augmentor
+import keras
+import numpy as np
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout, Input
+from keras.models import Sequential
+from keras import backend as K
+from six.moves import cPickle as Pickle
+import tensorflow as tf
+from tensorflow.python.tools import freeze_graph
+from tensorflow.python.tools import optimize_for_inference_lib
+from tensorflow.python.framework.graph_util import convert_variables_to_constants
+import cv2
+import csv
+import argparse
+from numpy import genfromtxt
+import operator
+from keras.models import load_model
+from functions import *
+
+before_data = 'temp_data'
+after_data = 'processed_data'
+image_size = 32
+pixel_depth = 255
+pickle_extension = '.pickle'
+num_classes = 48
+image_per_class = 500
+batch_size = 128
+num_classes = 48
+epochs = 12
+
+def get_folders(path):
+    data_folders = [os.path.join(path, d) for d in sorted(os.listdir(path))
+                    if os.path.isdir(os.path.join(path, d))]
+
+    if len(data_folders) != num_classes:
+        raise Exception(
+            'Expected %d folders, one per class. Found %d instead.' % (
+                num_classes, len(data_folders)))
+
+    return data_folders
+
+
+def load_letter(folder, min_num_images):
+    image_files = os.listdir(folder)
+    dataset = np.ndarray(shape=(len(image_files), image_size, image_size),
+                         dtype=np.float32)
+    print(folder)
+    image_index = -1
+    for image_index, image in enumerate(image_files):
+        image_file = os.path.join(folder, image)
+        try:
+            image_data = 1 * (cv2.imread(image_file, cv2.IMREAD_UNCHANGED).astype(float) > pixel_depth / 2)
+            if image_data.shape != (image_size, image_size):
+                raise Exception('Unexpected image shape: %s' % str(image_data.shape))
+            dataset[image_index, :, :] = image_data
+        except IOError as err:
+            print('Could not read:', image_file, ':', err, '- it\'s ok, skipping.')
+
+    num_images = image_index + 1
+    dataset = dataset[0:num_images, :, :]
+    if num_images < min_num_images:
+        raise Exception('Many fewer images than expected: %d < %d' % (num_images, min_num_images))
+
+    return dataset
+
+def maybe_pickle(data_folders, min_num_images_per_class, force=False):
+    dataset_names = []
+    for folder in data_folders:
+        set_filename = folder + pickle_extension
+        dataset_names.append(folder)
+        if os.path.exists(set_filename) and not force:
+            # You may override by setting force=True.
+            print('%s already present - Skipping pickling.' % set_filename)
+        else:
+            # print('Pickling %s.' % set_filename)
+            dataset = load_letter(folder, min_num_images_per_class)
+            try:
+                with open(set_filename, 'wb') as f:
+                    Pickle.dump(dataset, f, Pickle.HIGHEST_PROTOCOL)
+            except Exception as e:
+                print('Unable to save data to', set_filename, ':', e)
+
+    return dataset_names
+
+
+def make_arrays(nb_rows, img_size):
+    if nb_rows:
+        dataset = np.ndarray((nb_rows, img_size, img_size), dtype=np.float32)
+        labels = np.ndarray(nb_rows, dtype=np.int32)
+    else:
+        dataset, labels = None, None
+    return dataset, labels
+
+
+def merge_datasets(pickle_files, train_size, test_size=0, valid_size=0):
+    num_classes = len(pickle_files)
+    print(num_classes)
+    valid_dataset, valid_labels = make_arrays(valid_size, image_size)
+    test_dataset, test_labels = make_arrays(test_size, image_size)
+    train_dataset, train_labels = make_arrays(train_size, image_size)
+    valid_size_per_class = valid_size // num_classes
+    test_size_per_class = test_size // num_classes
+    train_size_per_class = train_size // num_classes
+
+    print(valid_size_per_class, test_size_per_class, train_size_per_class)
+
+    start_valid, start_test, start_train = 0, valid_size_per_class, (valid_size_per_class + test_size_per_class)
+    end_valid = valid_size_per_class
+    end_test = end_valid + test_size_per_class
+    end_train = end_test + train_size_per_class
+
+    print(start_valid, end_valid)
+    print(start_test, end_test)
+    print(start_train,end_train)
+
+    s_valid, s_test, s_train = 0, 0, 0
+    e_valid, e_test, e_train = valid_size_per_class, test_size_per_class, train_size_per_class
+    temp = []
+    for label, pickle_file in enumerate(pickle_files):
+        temp.append([label, pickle_file[-4:]])
+        try:
+            with open(pickle_file + pickle_extension, 'rb') as f:
+                letter_set = Pickle.load(f)
+                # let's shuffle the letters to have random validation and training set
+                np.random.shuffle(letter_set)
+                if valid_dataset is not None:
+                    valid_letter = letter_set[:end_valid, :, :]
+                    valid_dataset[s_valid:e_valid, :, :] = valid_letter
+                    valid_labels[s_valid:e_valid] = label
+                    s_valid += valid_size_per_class
+                    e_valid += valid_size_per_class
+
+                if test_dataset is not None:
+                    test_letter = letter_set[start_test:end_test, :, :]
+                    test_dataset[s_test:e_test, :, :] = test_letter
+                    test_labels[s_test:e_test] = label
+                    s_test += test_size_per_class
+                    e_test += test_size_per_class
+
+                train_letter = letter_set[start_train:end_train, :, :]
+                train_dataset[s_train:e_train, :, :] = train_letter
+                train_labels[s_train:e_train] = label
+                s_train += train_size_per_class
+                e_train += train_size_per_class
+        except Exception as e:
+            print('Unable to process data from', pickle_file, ':', e)
+            raise
+    with open('classes.csv', 'w') as my_csv:
+        writer = csv.writer(my_csv, delimiter=',')
+        writer.writerows(temp)
+    return valid_dataset, valid_labels, test_dataset, test_labels, train_dataset, train_labels
+
+def reformat(dataset, labels):
+    dataset = dataset.reshape((-1, 32, 32, 1)).astype(np.float32)
+    labels = (np.arange(num_classes) == labels[:, None]).astype(np.float32)
+    return dataset, labels
+
+def reformat(dataset, labels):
+    dataset = dataset.reshape((-1, 32, 32, 1)).astype(np.float32)
+    labels = (np.arange(num_classes) == labels[:, None]).astype(np.float32)
+    return dataset, labels
+
+def predict(img,model):
+    image_data = img
+    dataset = np.asarray(image_data)
+    dataset = dataset.reshape((-1, 32, 32, 1)).astype(np.float32)
+    a = model.predict(dataset)[0]
+    classes = np.genfromtxt('classes.csv', delimiter=',')[:, 1].astype(int)
+    new = dict(zip(classes, a))
+    res = sorted(new.items(), key=operator.itemgetter(1), reverse=True)
+    print(int(res[0][0]))
+    if res[0][1] < 1:
+        print("Other predictions")
+        for newtemp in res:
+            print(newtemp[0])
+
+
+"""
+WORKING FUNCTIONS
+"""
+
+def augmentor():
+    for f in list_folders(before_data):
+        if os.path.isdir(os.path.join(before_data, f, 'output')):
+            shutil.rmtree(os.path.join(before_data, f, 'output'))
+        p = Augmentor.Pipeline(os.path.join(before_data, f))
+        p.random_distortion(probability=1, grid_width=10, grid_height=10, magnitude=8)
+        p.sample(500, multi_threaded=True)
+
+def cleaner():
+    FOLDER_LIST = list_folders(before_data)
+    create_folders(after_data, FOLDER_LIST)
+    process_images(before_data, after_data, FOLDER_LIST)
+
+def processor():
+    data_folders = get_folders(after_data)
+    train_datasets = maybe_pickle(data_folders, image_per_class, True)
+    train_size = int(image_per_class * num_classes * 0.7)
+    test_size = int(image_per_class * num_classes * 0.2)
+    valid_size = int(image_per_class * num_classes * 0.1)
+    valid_dataset, valid_labels, test_dataset, test_labels, train_dataset, train_labels = merge_datasets(
+        train_datasets, train_size, test_size, valid_size)
+    pickle_file = 'data.pickle'
+    try:
+        f = open(pickle_file, 'wb')
+        save = {
+            'train_dataset': train_dataset,
+            'train_labels': train_labels,
+            'valid_dataset': valid_dataset,
+            'valid_labels': valid_labels,
+            'test_dataset': test_dataset,
+            'test_labels': test_labels,
+        }
+        Pickle.dump(save, f, Pickle.HIGHEST_PROTOCOL)
+        f.close()
+    except Exception as e:
+        print('Unable to save data to', pickle_file, ':', e)
+        raise
+    statinfo = os.stat(pickle_file)
+    print('Compressed pickle size:', statinfo.st_size)
+
+def train():
+    pickle_file = 'data.pickle'
+    with open(pickle_file, 'rb') as f:
+        save = Pickle.load(f)
+        train_dataset = save['train_dataset']
+        train_labels = save['train_labels']
+        valid_dataset = save['valid_dataset']
+        valid_labels = save['valid_labels']
+        test_dataset = save['test_dataset']
+        test_labels = save['test_labels']
+        del save  # hint to help gc free up memory
+        print('Training set', train_dataset.shape, train_labels.shape)
+        print('Validation set', valid_dataset.shape, valid_labels.shape)
+    train_dataset, train_labels = reformat(train_dataset, train_labels)
+    valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
+    test_dataset, test_labels = reformat(test_dataset, test_labels)
+    print('Training set', train_dataset.shape, train_labels.shape)
+    print('Validation set', valid_dataset.shape, valid_labels.shape)
+    print('Testing set', test_dataset.shape, test_labels.shape)
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(32, 32, 1)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
+    model.fit(train_dataset, train_labels, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(valid_dataset, valid_labels))
+    score = model.evaluate(test_dataset, test_labels, verbose=1)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+    model.save("model.h5")
+    """
+    (tf.train.Saver(),model,["conv2d_1_input"],"dense_2/Softmax")
+    
+    saver = tf.train.Saver()
+    tf.train.write_graph(K.get_session().graph_def,'out','model_graph.pbtxt')
+    saver.save(K.get_session(),'out/model.chkp')
+    freeze_graph.freeze_graph('out/model_graph.pbtxt',None,False,'out/model.chkp',"dense_2/Softmax","save/restore_all","save/Const:0",'out/frozen_model.pb',True,"")
+    
+    input_graph_def = tf.GraphDef()
+    with tf.gfile.Open('out/frozen_model.pb','rb') as f:
+        input_graph_def.ParseFromString(f.read())
+    output_graph_def = optimize_for_inference_lib.optimize_for_inference(input_graph_def,["conv2d_1_input"],["dense_2/Softmax"],tf.float32.as_datatype_enum)
+    
+    with tf.gfile.FastGFile('out/opt_model.pb','wb') as z:
+        z.write(output_graph_def.SerializeToString())
+        
+    print('graph Saved')
+    
+    session = K.get_session()
+    graph = session.graph
+    with graph.as_default():
+    """    
+    
+    
+    
+    
+
+def scan():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True,help="Path to the image to be scanned")
+    args = vars(ap.parse_args())
+    model = load_model("model.h5")
+    image = cv2.imread(args["image"], cv2.IMREAD_UNCHANGED)
+    if image.shape[2] == 4:
+        image = read_transparent_png(args["image"])
+    image = clean(image)
+    #cv2.imshow('gray', image)
+    #cv2.waitKey(0)
+    predict(image,model)
+
+def main():
+    exist = os.path.isfile('model.h5')
+    if(not exist):
+        augmentor()
+        cleaner()
+        processor()
+        train()
+        print("Training Completed")
+    scan()
+
+main()
---
+++ Web/check.php
@@ -0,0 +1,123 @@
+<?php
+
+if (isset($_POST['image'])) {
+    $img = $_POST['image'];
+    $img = str_replace(' ', '+', str_replace('data:image/png;base64,', '', $img));
+    $data = base64_decode($img);
+    $handle = fopen('./TEMP.png', 'w');
+    fwrite($handle, $data);
+    
+    exec("python Main.py -i TEMP.png > output.txt");
+
+    $myfile = fopen("output.txt", "r") or die("Unable to open file!");
+    $res =  fgets($myfile);
+    fclose($myfile);
+    $i = (int)$res;
+
+
+    if($i==3333)echo "അ	MALAYALAM LETTER A";
+    elseif($i==3334)echo "ആ	MALAYALAM LETTER AA";
+    elseif($i==3335)echo "ഇ	MALAYALAM LETTER I";
+    elseif($i==3336)echo "ഈ	MALAYALAM LETTER II";
+    elseif($i==3337)echo "ഉ	MALAYALAM LETTER U";
+    elseif($i==3338)echo "ഊ	MALAYALAM LETTER UU";
+    elseif($i==3339)echo "ഋ	MALAYALAM LETTER VOCALIC R";
+    elseif($i==3340)echo "ഌ	MALAYALAM LETTER VOCALIC L";
+    elseif($i==3342)echo "എ	MALAYALAM LETTER E";
+    elseif($i==3343)echo "ഏ	MALAYALAM LETTER EE";
+    elseif($i==3344)echo "ഐ	MALAYALAM LETTER AI";
+    elseif($i==3346)echo "ഒ	MALAYALAM LETTER O";
+    elseif($i==3347)echo "ഓ	MALAYALAM LETTER OO";
+    elseif($i==3348)echo "ഔ	MALAYALAM LETTER AU";
+    elseif($i==3349)echo "ക	MALAYALAM LETTER KA";
+    elseif($i==3350)echo "ഖ	MALAYALAM LETTER KHA";
+    elseif($i==3351)echo "ഗ	MALAYALAM LETTER GA";
+    elseif($i==3352)echo "ഘ	MALAYALAM LETTER GHA";
+    elseif($i==3353)echo "ങ	MALAYALAM LETTER NGA";
+    elseif($i==3354)echo "ച	MALAYALAM LETTER CA";
+    elseif($i==3355)echo "ഛ	MALAYALAM LETTER CHA";
+    elseif($i==3356)echo "ജ	MALAYALAM LETTER JA";
+    elseif($i==3357)echo "ഝ	MALAYALAM LETTER JHA";
+    elseif($i==3358)echo "ഞ	MALAYALAM LETTER NYA";
+    elseif($i==3359)echo "ട	MALAYALAM LETTER TTA";
+    elseif($i==3360)echo "ഠ	MALAYALAM LETTER TTHA";
+    elseif($i==3361)echo "ഡ	MALAYALAM LETTER DDA";
+    elseif($i==3362)echo "ഢ	MALAYALAM LETTER DDHA";
+    elseif($i==3363)echo "ണ	MALAYALAM LETTER NNA";
+    elseif($i==3364)echo "ത	MALAYALAM LETTER TA";
+    elseif($i==3365)echo "ഥ	MALAYALAM LETTER THA";
+    elseif($i==3366)echo "ദ	MALAYALAM LETTER DA";
+    elseif($i==3367)echo "ധ	MALAYALAM LETTER DHA";
+    elseif($i==3368)echo "ന	MALAYALAM LETTER NA";
+    elseif($i==3369)echo "ഩ	MALAYALAM LETTER NNNA";
+    elseif($i==3370)echo "പ	MALAYALAM LETTER PA";
+    elseif($i==3371)echo "ഫ	MALAYALAM LETTER PHA";
+    elseif($i==3372)echo "ബ	MALAYALAM LETTER BA";
+    elseif($i==3373)echo "ഭ	MALAYALAM LETTER BHA";
+    elseif($i==3374)echo "മ	MALAYALAM LETTER MA";
+    elseif($i==3375)echo "യ	MALAYALAM LETTER YA";
+    elseif($i==3376)echo "ര	MALAYALAM LETTER RA";
+    elseif($i==3377)echo "റ	MALAYALAM LETTER RRA";
+    elseif($i==3378)echo "ല	MALAYALAM LETTER LA";
+    elseif($i==3379)echo "ള	MALAYALAM LETTER LLA";
+    elseif($i==3380)echo "ഴ	MALAYALAM LETTER LLLA";
+    elseif($i==3381)echo "വ	MALAYALAM LETTER VA";
+    elseif($i==3382)echo "ശ	MALAYALAM LETTER SHA";
+    elseif($i==3383)echo "ഷ	MALAYALAM LETTER SSA";
+    elseif($i==3384)echo "സ	MALAYALAM LETTER SA";
+    elseif($i==3385)echo "ഹ	MALAYALAM LETTER HA";
+    elseif($i==3386)echo "ഺ	MALAYALAM LETTER TTTA";
+    elseif($i==3430)echo "൦	MALAYALAM DIGIT ZERO";
+    elseif($i==3431)echo "൧	MALAYALAM DIGIT ONE";
+    elseif($i==3432)echo "൨	MALAYALAM DIGIT TWO";
+    elseif($i==3433)echo "൩	MALAYALAM DIGIT THREE";
+    elseif($i==3434)echo "൪	MALAYALAM DIGIT FOUR";
+    elseif($i==3435)echo "൫	MALAYALAM DIGIT FIVE";
+    elseif($i==3436)echo "൬	MALAYALAM DIGIT SIX";
+    elseif($i==3437)echo "൭	MALAYALAM DIGIT SEVEN";
+    elseif($i==3438)echo "൮	MALAYALAM DIGIT EIGHT";
+    elseif($i==3439)echo "൯	MALAYALAM DIGIT NINE";
+    elseif($i==3440)echo "൰	MALAYALAM NUMBER TEN";
+    elseif($i==3441)echo "൱	MALAYALAM NUMBER ONE HUNDRED";
+    elseif($i==3442)echo "൲	MALAYALAM NUMBER ONE THOUSAND";
+    elseif($i==3443)echo "൳	MALAYALAM FRACTION ONE QUARTER";
+    elseif($i==3444)echo "൴	MALAYALAM FRACTION ONE HALF";
+    elseif($i==3445)echo "൵	MALAYALAM FRACTION THREE QUARTERS";
+    elseif($i==3449)echo "൹	MALAYALAM DATE MARK";
+    elseif($i==3450)echo "ൺ	MALAYALAM LETTER CHILLU NN";
+    elseif($i==3451)echo "ൻ	MALAYALAM LETTER CHILLU N";
+    elseif($i==3452)echo "ർ	MALAYALAM LETTER CHILLU RR";
+    elseif($i==3453)echo "ൽ	MALAYALAM LETTER CHILLU L";
+    elseif($i==3454)echo "ൾ	MALAYALAM LETTER CHILLU LL";
+    elseif($i==3455)echo "ൿ	MALAYALAM LETTER CHILLU K";
+    elseif($i==3461)echo "අ	SINHALA LETTER AYANNA";
+    elseif($i==3462)echo "ආ	SINHALA LETTER AAYANNA";
+    elseif($i==3463)echo "ඇ	SINHALA LETTER AEYANNA";
+    elseif($i==3464)echo "ඈ	SINHALA LETTER AEEYANNA";
+    elseif($i==3465)echo "ඉ	SINHALA LETTER IYANNA";
+    elseif($i==3466)echo "ඊ	SINHALA LETTER IIYANNA";
+    elseif($i==3467)echo "උ	SINHALA LETTER UYANNA";
+    elseif($i==3468)echo "ඌ	SINHALA LETTER UUYANNA";
+    elseif($i==3469)echo "ඍ	SINHALA LETTER IRUYANNA";
+    elseif($i==3470)echo "ඎ	SINHALA LETTER IRUUYANNA";
+    elseif($i==3471)echo "ඏ	SINHALA LETTER ILUYANNA";
+    elseif($i==3472)echo "ඐ	SINHALA LETTER ILUUYANNA";
+    elseif($i==3473)echo "එ	SINHALA LETTER EYANNA";
+    elseif($i==3474)echo "ඒ	SINHALA LETTER EEYANNA";
+    elseif($i==3475)echo "ඓ	SINHALA LETTER AIYANNA";
+    elseif($i==3476)echo "ඔ	SINHALA LETTER OYANNA";
+    elseif($i==3477)echo "ඕ	SINHALA LETTER OOYANNA";
+    elseif($i==3478)echo "ඖ	SINHALA LETTER AUYANNA";
+        
+    
+    /*shell_exec('cd ./ && /usr/bin/python ./Main.py -i ./TEMP.png 1> ./op.txt 2> ./error.txt');
+    
+    $h = fopen('./op.txt', 'r');
+    $result = array();
+    while (!feof($h)) {
+        array_push($result, fgets($h));
+    }
+    echo json_encode($result);
+} else {
+    echo 'ERROR';*/
+}
